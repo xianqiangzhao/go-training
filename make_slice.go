bb92577996d503372159c55fbbbf8704e4f14581
zhaoxianqiang <24930662@qq.com>
2019-07-05 14:22:52 +0800

    add

--- make_slice.go
+++ make_slice.go
@@ -15,6 +15,17 @@ func main() {
 
     d := c[2:5]
     printSlice("d", d)
+
+    var p *[]int = new([]int)
+    var v  []int = make([]int, 10,100)
+    fmt.Println(p,v)
+    if *p == nil {
+        fmt.Println("p is nil")
+    }
+    if v == nil {
+        fmt.Println("v is nil")
+    }
+
 }
 
 func printSlice(s string, x []int) {
